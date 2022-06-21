fd7db360374db8da07fc6e7e2b5b6cd6e876ab7a
Tzuyin Huang <jin19941231@gmail.com>
2022-06-21 16:38:21 +0800

    sql_done

--- www.py
+++ www.py
@@ -3,6 +3,32 @@ from bs4 import BeautifulSoup as bs
 import nums_from_string as nfs
 import re
 import pandas as pd
+import pymysql
+ 
+#連接mysql資料庫
+db = pymysql.connect(host='localhost',
+                    user='testuser',
+                    password='test123',
+                    database='project1')
+#建立操作游標
+cursor = db.cursor()
+#刪除可能資料表，避免資料錯誤
+#cursor.execute("DROP TABLE IF EXISTS sofy_pad_29")
+
+#執行www.py時才重新建立sofy_pad_29資料表
+if __name__ == '__main__':
+    cursor.execute('drop table if exists sofy_pad_29')
+    sql = """CREATE TABLE IF NOT EXISTS sofy_pad_29 (
+            market_name  CHAR(50) NOT NULL primary key,
+            goods_price  SMALLINT(10) UNSIGNED,
+            goods_num  SMALLINT(5) UNSIGNED,  
+            unit_price  FLOAT(2) UNSIGNED, 
+            sale_activity CHAR(10) DEFAULT '0',
+            timestamp TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP)"""
+    cursor.execute(sql)
+    db.close()
+
+
 all_markets=[]
 col=['通路名稱','商品價格','商品單位','單位價格','促銷活動有無']
 df=pd.DataFrame(all_markets,columns=col)
@@ -28,13 +54,13 @@ def all_in_1( market_name, url, price_tag, price_class_attr, unit_tag, unit_clas
         return market_list
 """
 def all_in_1( market_name, url, price_tag, price_class_attr, unit_tag, unit_class_attr, sale_tag='0', sale_class_attr='0' ): 
-    #url:目標網址，return bs物件
     
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
     web=requests.get(url,headers=headers)
     if web.status_code==200:   
         print('網路爬蟲成功')
         market_soup=bs(web.text,'lxml')
+
         #找商品價格
         goods_price=market_soup.find(price_tag,price_class_attr).text
         goods_price=nfs.get_nums(goods_price)
@@ -44,6 +70,7 @@ def all_in_1( market_name, url, price_tag, price_class_attr, unit_tag, unit_clas
         goods_title=market_soup.find(unit_tag,unit_class_attr).text
         result=re.findall('(\d+)片',goods_title)+re.findall('(\d+)PC',goods_title)+re.findall('(\d+)包',goods_title)+re.findall('(\d+)入',goods_title)
         unit_num=1
+        #查看是否有影響單位的活動，如果有unit_num*2
         if (sale_tag!='0') or (sale_class_attr!='0'):
             event=market_soup.find(sale_tag,sale_class_attr).text
             if event.find('買一送一')!=-1:
@@ -56,7 +83,22 @@ def all_in_1( market_name, url, price_tag, price_class_attr, unit_tag, unit_clas
                 unit_num*=int(i)
             sales10='0'
 
-        market_list=[market_name , goods_price , unit_num , round(goods_price/unit_num,2),sales10]
+        # #將資料寫入sofy_pad_29資料表
+        inserting_sql = """
+                INSERT INTO sofy_pad_29
+                (market_name,goods_price,goods_num,unit_price,sale_activity)
+                VALUES ('{}',{},{},{},'{}')
+                """.format(
+                market_name , goods_price , unit_num , round(goods_price/unit_num,2),sales10,
+                market_name , goods_price , unit_num , round(goods_price/unit_num,2),sales10)
+        
+        # 執行sql
+        cursor.execute(inserting_sql)
+        # 提交到sql中執行
+        db.commit() 
+        db.close()
+
+        market_list=[market_name , goods_price , unit_num , round(goods_price/unit_num,2),sales10]        
         return market_list
 
 
