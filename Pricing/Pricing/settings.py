13c5a18748d5befb7328552e49f658af01c1b641
Tzuyin Huang <jin19941231@gmail.com>
2022-07-11 16:30:23 +0800

    cant connect with heroku

--- Pricing/Pricing/settings.py
+++ Pricing/Pricing/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,10 +25,7 @@ SECRET_KEY = 'django-insecure-+gzfwi#ab8go2101klc-$h6^!o4ouas1y4go_fm0gnmh)(j_$b
 DEBUG = True
 
 ALLOWED_HOSTS = []
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'urexpenses.herokuapp.com/'
-]
+
 
 # Application definition
 
@@ -51,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Pricing.urls'
@@ -126,7 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
