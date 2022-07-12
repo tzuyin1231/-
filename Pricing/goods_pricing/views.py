607bffcdfeee1503bd4bb8f9cdc02c4ec114aa3c
Tzuyin Huang <jin19941231@gmail.com>
2022-07-12 15:29:17 +0800

    新增實體店面搜尋

--- Pricing/goods_pricing/views.py
+++ Pricing/goods_pricing/views.py
@@ -4,5 +4,6 @@ from goods_pricing.models import sofy_pad_29    #插入表
 
 def table(request):
   #table_form=forms.SignupForm()  #樣式 ，在forms.py裡配置好了
-  markets=sofy_pad_29.objects.all()  #獲取我們的資料庫資訊到names裡
+  markets=sofy_pad_29.objects.all()  #獲取我們的資料庫資訊
+  google_markets=['康是美','屈臣氏','寶雅','家樂福','全聯']
   return render(request,"markets.html",locals()) #必須用這個return
